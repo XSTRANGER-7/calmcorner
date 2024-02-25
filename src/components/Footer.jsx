00db5afc05f7a2a128e21277ad8b4fd7d7cd274f
vinayak3010 <102256177+vinayak3010@users.noreply.github.com>
2024-02-25 22:05:43 +0530

    Update Footer.jsx
    added chatbot

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,3 @@
-// import React from "react";
-// import audi from "../assests/audi1.mp3";
-// import { AiOutlineGithub, AiOutlineInstagram, AiOutlineLinkedin, AiOutlineMail, AiOutlineTwitter, AiOutlineWhatsApp } from "react-icons/ai";
-// const Footer = () => {
-//   return (
-//     <footer>
-//       <div className="lefty">
-//         <h2>HeiwaHabu</h2>
-//         <p>@all rights reserved</p>
-//         <h3>Follow us on </h3>
-//         <p>HeiwaHabu@gmail.com</p>
-//       </div>
-//       <div className="righty">
-//         <AiOutlineMail />
-//         <AiOutlineWhatsApp />
-//         <AiOutlineTwitter/>
-//         <AiOutlineGithub/>
-//         <AiOutlineInstagram/>
-//         <AiOutlineLinkedin/>
-//       </div>
-//       <audio controls autoPlay loop preload="auto" src={audi}/>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
 
 import React, { useState } from "react";
 import { AiOutlineMail, AiOutlineWhatsApp, AiOutlineTwitter, AiOutlineGithub, AiOutlineInstagram, AiOutlineLinkedin } from "react-icons/ai";
